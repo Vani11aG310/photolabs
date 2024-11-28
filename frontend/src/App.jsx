af0686567d4b1a4d6aae9c0c963cb0afeebce64d
Angelo <angvil02@gmail.com>
2024-11-28 14:27:45 -0800

    added topics to the nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 import './styles/PhotoList.scss'
 import PhotoList from 'components/PhotoList';
+import TopicList from 'components/TopicList';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  // const photos = [1, 2, 3].map((key) => {
-  //   return (
-  //     <PhotoListItem key={key} photo={sampleDataForPhotoListItem}/>
-  //   )
-  // })
   return (
     <div className="App">
-      <div className='photo-list'>
-        <PhotoList />
-      </div>
+      <TopicList />
+      <PhotoList />
     </div>
   );
 };
