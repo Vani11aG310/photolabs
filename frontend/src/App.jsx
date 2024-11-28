8c20ef7a47b17d1ab06586ded199ccf71ab38903
Angelo <angvil02@gmail.com>
2024-11-28 14:54:10 -0800

    Implemented the Home Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 
 import './App.scss';
 import './styles/PhotoList.scss'
-import PhotoList from 'components/PhotoList';
-import TopicList from 'components/TopicList';
-import TopNavigation from 'components/TopNavigationBar';
+import HomeRoute from 'routes/HomeRoute';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <TopNavigation />
-      <PhotoList />
+      <HomeRoute />
     </div>
   );
 };
