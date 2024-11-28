0e1fe672dc9ed89491e9fd4cb481e18c6a77aa78
Angelo <angvil02@gmail.com>
2024-11-27 19:58:23 -0800

    added the like button to PhotoListItem component

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,11 +4,16 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  const [like, setLike] = useState(false)
+  const clickHandler = () => {
+    setLike(like === false ? true : false)
+    console.log(like)
+  }
   return (
-    <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
-      </div>
+    <div className="photo-list__fav-icon" onClick={clickHandler}>
+        <div className="photo-list__fav-icon-svg">
+          <FavIcon selected={like}/>
+        </div>
     </div>
   );
 }
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = (props) => {
   /* Insert React */
   const { photo } = props
   return (
     <div className="photo-list__item">
+      <PhotoFavButton />
       <img src={photo.imageSource} className="photo-list__image" alt="main image" />
       <div className="photo-list__user-details">
         <img src={photo.profile} className="photo-list__user-profile" alt="profile pic" />
