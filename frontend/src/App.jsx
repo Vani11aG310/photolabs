2e65bb32620fddc4205462355d921a5a0a04c10f
Angelo <angvil02@gmail.com>
2024-11-28 15:05:13 -0800

    refactored app to use the mock data in mock folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 import './App.scss';
 import './styles/PhotoList.scss'
 import HomeRoute from 'routes/HomeRoute';
+import photos from 'mocks/photos';
+import topics from 'mocks/topics';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const photosData = photos;
+  const topicsData = topics;
   return (
     <div className="App">
-      <HomeRoute />
+      <HomeRoute photos={photosData} topics={topicsData}/>
     </div>
   );
 };
--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -3,26 +3,11 @@ import React from "react";
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
 
-const TopicList = () => {
-  const topicListArray = sampleDataForTopicList.map(topic => {
+
+const TopicList = (props) => {
+  const { topics } = props; 
+  const topicListArray = topics.map(topic => {
     return <TopicListItem key={topic.id} title={topic.title}/>
   })
   return (
